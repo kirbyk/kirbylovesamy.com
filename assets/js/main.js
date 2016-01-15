c086721da30ec9b68bfc7fc2125c923aa5feaf6a
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2016-01-15 18:30:17 -0500

    Finialize bride and groom

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 $(function() {
   registerNavButtons();
+
+  mapboxgl.accessToken = 'pk.eyJ1Ijoia2lyYnkiLCJhIjoiY2lqZzd0d3FlMDE5d3RobTV0NW9hNjM4OCJ9.a_Ro4knFLUPjaQG3cTx3fw';
+  var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
+    center: [-74.50, 40], // starting position
+    zoom: 9 // starting zoom
+  });
 });
 
 function registerNavButtons() {
