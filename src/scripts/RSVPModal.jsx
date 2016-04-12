f778200c7b525c0dba372906c5550cd16e7d8625
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2016-04-11 20:42:45 -0400

    Implement modal logic

--- src/scripts/RSVPModal.jsx
+++ src/scripts/RSVPModal.jsx
@@ -2,13 +2,43 @@ import React from 'react';
 
 
 export default class RSVPModal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      people: []
+    };
+
+    this.state.people = this.props.attendee.people.map((person) => ({
+      name: person,
+      status: 'Unknown'
+    }));
+  }
+
+  toggleStatus(index) {
+    var person = this.state.people[index];
+
+    if (person.status !== 'Attending') {
+      this.state.people[index].status = 'Attending';
+    } else {
+      this.state.people[index].status = 'Not Attending';
+    }
+
+    this.setState(this.state);
+  }
+
+  submit() {
+    console.log(this.state);
+  }
+
   render() {
-    console.log(this.props.attendee);
     return (
       <div>
         <h3 className='center primary-font'>
           { this.props.attendee.formatted }
         </h3>
+
+        <br />
 
         <p className='center'>
           Please modify the attendee(s) below to reflect your RSVP.
@@ -17,19 +47,21 @@ export default class RSVPModal extends React.Component {
         <div className='row'>
           <div className='col-xs-8 col-xs-offset-2'>
             <ul className='list-group'>
-              {this.props.attendee.people.map((person, index) => (
-                <li className='list-group-item' key={index}>
-                  <span className='badge'>Unknown</span>
-                  { person }
+
+              {this.state.people.map((person, index) => (
+                <li className='list-group-item' key={index} onClick={ () => this.toggleStatus(index) }>
+                  <span className='badge'>{ person.status }</span>
+                  { person.name }
                 </li>
               ))}
+
             </ul>
           </div>
         </div>
 
         <div className='row'>
           <div className='col-xs-6 col-xs-offset-3 center'>
-            <input className='btn btn-default contact-submit' id='rsvp-submit' type='submit' value='Submit RSVP' />
+            <input className='btn btn-default contact-submit' id='rsvp-submit' type='submit' value='Submit RSVP' onClick={ () => this.submit() } />
           </div>
         </div>
       </div>
